f4d62431bc83499d22af6d76b6ce0e8bac7f98b9
Marcin Ligmann <marcinligmann2@gmail.com>
2024-04-18 15:34:41 +0200

    initial working app to decode text from qrcode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import jsQR from 'jsqr';
 
 function App() {
+  const [qrCodeData, setQRCodeData] = useState('');
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const img = new Image();
+      img.onload = function() {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const qrCode = jsQR(imageData.data, imageData.width, imageData.height);
+        if (qrCode) {
+          setQRCodeData(qrCode.data);
+          console.log(qrCode);
+        } else {
+          setQRCodeData("No QR code found.");
+        }
+      };
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Simple QR scaner</h1>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      
+      {qrCodeData && (
+        <>
+          <p>output:</p>
+          <div id="output">{qrCodeData}</div>
+        </>
+      )}
+    </>
   );
 }
 
